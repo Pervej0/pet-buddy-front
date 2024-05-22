b60439069261f71ef131e3272e31699da7ff1d44
pervej0 <mdparvez222khan@gmail.com>
2024-05-22 12:16:02 +0600

    mui theme setup

---
+++ src/lib/theme/theme.tsx
@@ -0,0 +1,67 @@
+"use client";
+import { Fira_Sans, Roboto } from "next/font/google";
+import { createTheme } from "@mui/material/styles";
+
+const roboto = Roboto({
+  weight: ["300", "400", "500", "700"],
+  subsets: ["latin"],
+  display: "swap",
+});
+
+const firaSans = Fira_Sans({
+  weight: ["300", "400", "500", "700"],
+  subsets: ["latin"],
+  display: "auto",
+});
+
+const theme = createTheme({
+  palette: {
+    primary: {
+      main: "#1586FD",
+    },
+    secondary: {
+      main: "#666F73",
+    },
+  },
+  components: {
+    MuiButton: {
+      defaultProps: {
+        variant: "contained",
+      },
+      styleOverrides: {
+        root: {
+          padding: "8px 24px",
+        },
+      },
+    },
+    MuiContainer: {
+      defaultProps: {
+        maxWidth: "lg",
+      },
+    },
+  },
+  typography: {
+    fontFamily: firaSans.style.fontFamily,
+    h1: {
+      fontFamily: roboto.style.fontFamily,
+    },
+    h2: {
+      fontFamily: roboto.style.fontFamily,
+    },
+    h3: {
+      fontFamily: roboto.style.fontFamily,
+    },
+    h4: {
+      fontFamily: roboto.style.fontFamily,
+    },
+    h5: {
+      fontFamily: roboto.style.fontFamily,
+    },
+    body1: {
+      fontFamily: firaSans.style.fontFamily,
+      color: "#0B1134",
+    },
+  },
+});
+
+export default theme;
