72c5669dce4fca0676833dbf32169bfb0ff35560
pervej0 <mdparvez222khan@gmail.com>
2024-05-23 15:35:14 +0600

    napbar responsive done

--- src/components/Shared/Navbar.tsx
+++ src/components/Shared/Navbar.tsx
@@ -3,58 +3,145 @@
 import assets from "@/assets";
 // import logOutUser from "@/services/actions/logOutUser";
 import { getUserInfo } from "@/services/auth.services";
-import { Box, Button, Container, Stack, Typography } from "@mui/material";
+import {
+  AppBar,
+  Box,
+  Button,
+  Container,
+  CssBaseline,
+  Divider,
+  Drawer,
+  IconButton,
+  List,
+  ListItem,
+  ListItemButton,
+  ListItemText,
+  MenuItem,
+  Stack,
+  Toolbar,
+  Typography,
+} from "@mui/material";
 import Image from "next/image";
 import Link from "next/link";
-import { useRouter } from "next/navigation";
 import React from "react";
+import ProfileDropdown from "./ProfileDropdown";
+import MenuIcon from "@mui/icons-material/Menu";
 
-const Navbar = () => {
-  const user = getUserInfo();
-  const router = useRouter();
+interface Props {
+  window?: () => Window;
+}
+
+const drawerWidth = 240;
+const navItems = ["Home", "About", "Contact"];
+
+const Navbar = (props: Props) => {
+  const { window } = props;
+  const [mobileOpen, setMobileOpen] = React.useState(false);
+
+  const handleDrawerToggle = () => {
+    setMobileOpen((prevState) => !prevState);
+  };
+
+  const drawer = (
+    <Box sx={{ textAlign: "center" }}>
+      <Box sx={{ display: "flex", justifyContent: "center", py: 2 }}>
+        <Image
+          src={assets.images.brand}
+          alt="Brand logo"
+          width={120}
+          height={120}
+        />
+      </Box>
+      <Divider />
+      <List>
+        {navItems.map((item) => (
+          <ListItem key={item} disablePadding>
+            <ListItemButton sx={{ textAlign: "center" }}>
+              <ListItemText primary={item} />
+            </ListItemButton>
+          </ListItem>
+        ))}
+      </List>
+      <Box>
+        <ProfileDropdown />
+      </Box>
+    </Box>
+  );
+
+  const container =
+    window !== undefined ? () => window().document.body : undefined;
 
   return (
-    <Container>
-      <Stack py={3} direction="row" justifyContent="space-between">
-        <Box>
-          <Image
-            src={assets.images.brand}
-            alt="Brand logo"
-            width={120}
-            height={120}
-          />
-        </Box>
-        <Stack
-          direction="row"
-          justifyContent="space-between"
-          alignItems="center"
-          gap={4}
-        >
-          <Typography component={Link} href="/">
-            Home
-          </Typography>
-          <Typography>About Us</Typography>
-          <Typography>Team</Typography>
-          <Typography>Contact us</Typography>
-        </Stack>
-        <Box>
-          {!user ? (
-            <Button LinkComponent={Link} href="/login">
-              Login
-            </Button>
-          ) : (
-            <Button
-              // onClick={() => logOutUser(router)}
-              color="error"
-              LinkComponent={Link}
-              variant="contained"
+    <Box sx={{ display: "flex" }}>
+      {/* <CssBaseline /> */}
+      <AppBar component="nav" sx={{ backgroundColor: "#fefcff" }}>
+        <Container>
+          <Stack
+            py={1}
+            direction="row"
+            justifyContent="space-between"
+            alignItems="center"
+          >
+            <IconButton
+              color="inherit"
+              aria-label="open drawer"
+              edge="start"
+              onClick={handleDrawerToggle}
+              sx={{ mr: 2, display: { sm: "none" } }}
             >
-              Logout
-            </Button>
-          )}
-        </Box>
-      </Stack>
-    </Container>
+              <MenuIcon />
+            </IconButton>
+            <Box>
+              <Image
+                src={assets.images.brand}
+                alt="Brand logo"
+                width={120}
+                height={120}
+              />
+            </Box>
+            <Stack
+              direction="row"
+              justifyContent="space-between"
+              alignItems="center"
+              gap={4}
+              sx={{ display: { xs: "none", sm: "block", md: "inherit" } }}
+            >
+              {navItems.map((item, index) => (
+                <Typography key={index} component={Link} href="/">
+                  {item}
+                </Typography>
+              ))}
+            </Stack>
+            <Box sx={{ display: { xs: "none", sm: "block" } }}>
+              <ProfileDropdown />
+            </Box>
+          </Stack>
+        </Container>
+      </AppBar>
+      <nav>
+        <Drawer
+          container={container}
+          variant="temporary"
+          open={mobileOpen}
+          onClose={handleDrawerToggle}
+          ModalProps={{
+            keepMounted: true, // Better open performance on mobile.
+          }}
+          sx={{
+            display: { xs: "block", sm: "none" },
+            "& .MuiDrawer-paper": {
+              boxSizing: "border-box",
+              width: drawerWidth,
+            },
+          }}
+        >
+          {drawer}
+        </Drawer>
+      </nav>
+      <Box component="main" sx={{ p: 3 }}>
+        {/* <Toolbar /> */}
+      </Box>
+    </Box>
   );
 };
 
---
+++ src/components/Shared/ProfileDropdown.tsx
@@ -0,0 +1,127 @@
+import React from "react";
+import { Dropdown } from "@mui/base/Dropdown";
+import { Menu } from "@mui/base/Menu";
+import { MenuButton } from "@mui/base/MenuButton";
+import { MenuItem as BaseMenuItem, menuItemClasses } from "@mui/base/MenuItem";
+import {
+  AppBar,
+  Avatar,
+  Box,
+  Button,
+  ClickAwayListener,
+  Grow,
+  IconButton,
+  MenuItem,
+  MenuList,
+  Paper,
+  Popper,
+  Toolbar,
+  Tooltip,
+  Typography,
+  styled,
+} from "@mui/material";
+import AccountCircleIcon from "@mui/icons-material/AccountCircle";
+import { AccountCircle, Padding } from "@mui/icons-material";
+import { getUserInfo } from "@/services/auth.services";
+import { useRouter } from "next/navigation";
+import Link from "next/link";
+
+const ProfileDropdown = () => {
+  const user = getUserInfo();
+  const router = useRouter();
+  const [open, setOpen] = React.useState(false);
+  const anchorRef = React.useRef<HTMLButtonElement>(null);
+
+  const handleToggle = () => {
+    setOpen((prevOpen) => !prevOpen);
+  };
+
+  const handleClose = (event: Event | React.SyntheticEvent) => {
+    if (
+      anchorRef.current &&
+      anchorRef.current.contains(event.target as HTMLElement)
+    ) {
+      return;
+    }
+
+    setOpen(false);
+  };
+
+  function handleListKeyDown(event: React.KeyboardEvent) {
+    if (event.key === "Tab") {
+      event.preventDefault();
+      setOpen(false);
+    } else if (event.key === "Escape") {
+      setOpen(false);
+    }
+  }
+
+  // return focus to the button when we transitioned from !open -> open
+  const prevOpen = React.useRef(open);
+  React.useEffect(() => {
+    if (prevOpen.current === true && open === false) {
+      anchorRef.current!.focus();
+    }
+
+    prevOpen.current = open;
+  }, [open]);
+
+  return (
+    <>
+      {!user ? (
+        <Button LinkComponent={Link} href="/login">
+          Login
+        </Button>
+      ) : (
+        <div>
+          <Button
+            ref={anchorRef}
+            sx={{ backgroundColor: "#ffffff", color: "#000000" }}
+            id="composition-button"
+            aria-controls={open ? "composition-menu" : undefined}
+            aria-expanded={open ? "true" : undefined}
+            aria-haspopup="true"
+            onClick={handleToggle}
+          >
+            My Profile
+          </Button>
+          <Popper
+            open={open}
+            anchorEl={anchorRef.current}
+            role={undefined}
+            placement="bottom-start"
+            transition
+            disablePortal
+          >
+            {({ TransitionProps, placement }) => (
+              <Grow
+                {...TransitionProps}
+                style={{
+                  transformOrigin:
+                    placement === "bottom-start" ? "left top" : "left bottom",
+                }}
+              >
+                <Paper>
+                  <ClickAwayListener onClickAway={handleClose}>
+                    <MenuList
+                      autoFocusItem={open}
+                      id="composition-menu"
+                      aria-labelledby="composition-button"
+                      onKeyDown={handleListKeyDown}
+                    >
+                      <MenuItem onClick={handleClose}>Profile</MenuItem>
+                      <MenuItem onClick={handleClose}>My account</MenuItem>
+                      <MenuItem onClick={handleClose}>Logout</MenuItem>
+                    </MenuList>
+                  </ClickAwayListener>
+                </Paper>
+              </Grow>
+            )}
+          </Popper>
+        </div>
+      )}
+    </>
+  );
+};
+
+export default ProfileDropdown;
