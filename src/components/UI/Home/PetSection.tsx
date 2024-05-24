02917070d97edf5ebe461fc62329c79620b4dae7
pervej0 <mdparvez222khan@gmail.com>
2024-05-24 12:48:47 +0600

    pagination added

--- src/components/UI/Home/PetSection.tsx
+++ src/components/UI/Home/PetSection.tsx
@@ -5,15 +5,26 @@ import {
   Box,
   Container,
   Grid,
+  Pagination,
   Stack,
   TextField,
   Typography,
 } from "@mui/material";
 import React from "react";
+import PetCard from "./PetCard";
+import { TPet } from "@/types/common.type";
 
 const PetSection = () => {
-  const { data, isLoading } = useGetAllPetsQuery({});
-  console.log(data, "x");
+  const [page, setPage] = React.useState(1);
+  const { data: pets, isLoading } = useGetAllPetsQuery({
+    page: page,
+    limit: 9,
+  });
+
+  const handleChange = (event: React.ChangeEvent<unknown>, value: number) => {
+    setPage(value);
+  };
+  console.log(pets, "x");
   return (
     <Box my={5}>
       <Container>
@@ -21,15 +32,24 @@ const PetSection = () => {
           direction="row"
           alignItems="center"
           justifyContent="space-between"
+          mb={8}
         >
           <Typography variant="h5" component="h5" fontWeight={500}>
             See All Pets Here
           </Typography>
           <TextField label="Search pets" size="small" />
         </Stack>
-        <Grid container>
-          <Grid item></Grid>
+        <Grid container spacing={3}>
+          {pets?.data.map((item: TPet) => (
+            <Grid item key={item.id} md={4} sm={6} xs={12}>
+              <PetCard petDetails={item} />
+            </Grid>
+          ))}
         </Grid>
+        <Stack spacing={2} mt={4}>
+          <Typography>Page: {page}</Typography>
+          <Pagination count={10} page={page} onChange={handleChange} />
+        </Stack>
       </Container>
     </Box>
   );
---
+++ src/types/common.type.ts
@@ -0,0 +1,15 @@
+export interface TPet {
+  id: string;
+  name: string;
+  species: string;
+  breed: string;
+  age: number;
+  size: string;
+  location: string;
+  description: string;
+  temperament: string;
+  medicalHistory: string;
+  adoptionRequirements: string;
+  createdAt: string;
+  updatedAt: string;
+}
